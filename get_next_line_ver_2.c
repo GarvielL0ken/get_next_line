6197c3352ab67d2d5286a325f9af9c005924ac12
Jed Sarkis <jsarkis@c3r7s8.wethinkcode.co.za>
2019-06-06 13:01:05 +0200

    get_next_line_ver_2 working with BUFF_SIZE = 1

--- get_next_line_ver_2.c
+++ get_next_line_ver_2.c
@@ -6,7 +6,7 @@
 /*   By: jsarkis <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/04 12:03:55 by jsarkis           #+#    #+#             */
-/*   Updated: 2019/06/04 18:29:45 by jsarkis          ###   ########.fr       */
+/*   Updated: 2019/06/06 12:59:56 by jsarkis          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,26 +30,56 @@ int ft_find_index(char *str, char to_find)
 
 int	get_next_line(const int fd, char **line)
 {
-	int i;
-	int pos;
-	static char *buff;
+	int			i;
+	int			j;
+	int			pos;
+	static char	*buff;
 
 	if (!buff)
 	{
 		buff = (char *)malloc(BUFF_SIZE + 1);
-		buff[0] = '\0';
 		buff[BUFF_SIZE] = '\0';
-	}
-	if (buff[0] == '\0')
-		read(fd, buff, BUFF_SIZE);
-	i = 0;
-	while (i < BUFF_SIZE && buff[i] != '\n')
-	{
-		*(*line + i) = buff[i];
-		i++;
-	}
-	if (ft_find_index(buff, '\n') == -1)
 		buff[0] = '\0';
+	}
+	i = 0;
+	j = BUFF_SIZE;
+	while (j == BUFF_SIZE && buff[j] != '\n')
+	{
+		if (buff[0] == '\0')
+			read(fd, buff, BUFF_SIZE);
+		j = 0;
+		while (buff[j] != '\0')
+		{
+			if (buff[j] != '\n')
+				printf("buff[%d] = %c\n", j, buff[j]);
+			else
+				printf("buff[%d] = \\n\n", j);
+			j++;
+		}
+		j = 0;
+		while (buff[j] != '\n' && j < BUFF_SIZE && buff[j] != '\0')
+		{
+			*(*line + i) = buff[j];
+			printf("line[%d] = %c\n", i, *(*line + i));
+			i++;
+			j++;
+		}
+		pos = ft_find_index(buff, '\n');
+		if (pos == -1 || buff[j] == '\0')
+			buff[0] = '\0';
+		else
+		{
+			j = 0;
+			pos++;
+			while (buff[pos + j] != '\0')
+			{
+				buff[j] = buff[pos + j];
+				j++;
+			}
+			buff[j] = '\0';
+		}
+	}
+	return (0);
 }
 
 int	main()
@@ -60,18 +90,21 @@ int	main()
 	char	*str_3;
 
 	fd = open("test", 'r');
-	str_1 = (char *)malloc(BUFF_SIZE + 1);
-	str_2 = (char *)malloc(BUFF_SIZE + 1);
-	str_3 = (char *)malloc(BUFF_SIZE + 1);
+	str_1 = (char *)malloc(64);
+	str_1[63] = '\0';
+	str_2 = (char *)malloc(64);
+	str_2[63] = '\0';
+	str_3 = (char *)malloc(64);
+	str_3[63] = '\0';
 
 	get_next_line(fd, &str_1);
 	get_next_line(fd, &str_2);
 	get_next_line(fd, &str_3);
-	printf("result: \n");
+	printf("\nresult: \n");
 	printf("String 1: %s\n", str_1);
 	printf("String 2: %s\n", str_2);
 	printf("String 3: %s\n", str_3);
-	printf("%s", str_1);
-	printf("%s", str_2);
-	printf("%s", str_3);
+	//printf("%s", str_1);
+	//printf("%s", str_2);
+	//printf("%s", str_3);
 }
